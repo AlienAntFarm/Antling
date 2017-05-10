a6a28613ede0d87f8b4d330916b621dea2f882e4
Maxime Vidori <maxime.vidori@gmail.com>
2017-05-10 21:28:08 +0200

    Retrieve job state from json

--- client/job.go
+++ client/job.go
@@ -4,7 +4,25 @@ type job struct {
 	*endpoint
 }
 
+// >>> this as to be shared
+const (
+	JOB_NEW = iota
+	JOB_PENDING
+	JOB_FINISH
+	JOB_ERROR
+)
+
+var JOB_STATES = [...]string{
+	"NEW",
+	"PENDING",
+	"FINISH",
+	"ERROR",
+}
+
+// <<< this as to be shared
+
 type Job struct {
 	Id       int `json:"id"`
+	State    int `json:"state"`
 	endpoint *job
 }
