6c31f8f186cc5f79a5e34073b57e082e394a23e8
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-05 19:45:47 +0200

    Use named pipes to communicate with containers

--- lxc/runtime.go
+++ lxc/runtime.go
@@ -5,53 +5,21 @@ import (
 	"github.com/alienantfarm/antling/utils"
 	"github.com/golang/glog"
 	"gopkg.in/lxc/go-lxc.v2"
-	"io/ioutil"
-	"os"
+	"path"
 )
 
-type pipes struct {
-	stdin, stderr, stdout, logs *os.File
-}
-
-func newPipes() (p *pipes, err error) {
-	p = &pipes{}
-
-	if p.stdin, err = os.Open(os.DevNull); err != nil {
-		return
-	}
-	for _, pipe := range []**os.File{&p.stderr, &p.stdout, &p.logs} {
-		if *pipe, err = ioutil.TempFile("", ""); err != nil {
-			return
-		}
-	}
-	return
-}
-
-func (p *pipes) Close() {
-	p.stdin.Close()
-	for _, pipe := range []*os.File{p.stderr, p.stdout, p.logs} {
-		pipe.Close()
-		if err := os.Remove(pipe.Name()); err != nil {
-			glog.Errorf("deleting custom pipe failed %s", err)
-		}
-	}
-}
-
-func (p *pipes) Log() error {
-	for i, pipe := range []*os.File{p.stderr, p.stdout, p.logs} {
-		pipe.Seek(0, os.SEEK_SET)
-		if b, err := ioutil.ReadAll(pipe); err != nil {
-			return err
-		} else {
-			glog.Infof("%s:\n %s", []string{"stderr", "stdout", "logs"}[i], b)
-		}
-	}
-	return nil
+var LOG_LEVELS = [...]lxc.LogLevel{
+	lxc.CRIT,
+	lxc.ERROR,
+	lxc.WARN,
+	lxc.NOTICE,
+	lxc.INFO,
+	lxc.TRACE,
 }
 
 func Start(job *structs.Job) error {
 	// open temporary files for default pipes
-	p, err := newPipes()
+	p, err := newPipes(job.Image.Archive)
 	if err != nil {
 		glog.Infof("opening temp files for container output failed")
 		return err
@@ -64,13 +32,12 @@ func Start(job *structs.Job) error {
 		return err
 	}
 	c.SetLogLevel(LOG_LEVELS[utils.LogLevel()]) // pass log level to container
-	c.SetLogFile(p.logs.Name())
-	defer p.Log()
+	c.SetLogFile(path.Join(p.dir, "logs"))
+
 	options := lxc.DefaultAttachOptions
 	options.StdinFd = p.stdin.Fd()
 	options.StdoutFd = p.stdout.Fd()
 	options.StderrFd = p.stderr.Fd()
-
 	if err := c.Start(); err != nil {
 		return err
 	}
--- lxc/utils.go
+++ lxc/utils.go
@@ -1,14 +1,71 @@
 package lxc
 
 import (
-	"gopkg.in/lxc/go-lxc.v2"
+	"bufio"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"os"
+	"path"
+	"syscall"
 )
 
-var LOG_LEVELS = [...]lxc.LogLevel{
-	lxc.CRIT,
-	lxc.ERROR,
-	lxc.WARN,
-	lxc.NOTICE,
-	lxc.INFO,
-	lxc.TRACE,
+type pipes struct {
+	stdin, stderr, stdout *os.File
+	dir                   string
+}
+
+func pipeReading(path string) {
+	glog.Infof("opening %s for reading", path)
+	if pipe, err := os.OpenFile(path, os.O_RDONLY, 0600); err != nil {
+		glog.Errorf("opening %s failed, %s", path, err)
+	} else {
+		defer pipe.Close()
+		scanner := bufio.NewScanner(pipe)
+		for scanner.Scan() {
+			glog.Infoln(scanner.Text()) // Println will add back the final '\n'
+		}
+		glog.Infof("closing reading scanner on %s", path)
+	}
+}
+
+func initPipe(path string) (*os.File, error) {
+	if err := syscall.Mkfifo(path, 0600); err != nil {
+		return nil, err
+	}
+	go pipeReading(path)
+	glog.Infof("opening for %s writing", path)
+	return os.OpenFile(path, os.O_WRONLY, 0600)
+}
+
+func (p *pipes) Close() {
+	for _, pipe := range []*os.File{p.stdin, p.stderr, p.stdout} {
+		pipe.Close()
+	}
+	os.RemoveAll(p.dir)
+}
+
+func newPipes(prefix string) (p *pipes, err error) {
+	p = &pipes{}
+
+	if p.stdin, err = os.Open(os.DevNull); err != nil {
+		return
+	}
+	if p.dir, err = ioutil.TempDir("", "antling."+prefix); err != nil {
+		return
+	}
+	defer utils.RemoveOnFail(p.dir, err)
+
+	if p.stderr, err = initPipe(path.Join(p.dir, "stderr")); err != nil {
+		return
+	}
+	if p.stdout, err = initPipe(path.Join(p.dir, "stdout")); err != nil {
+		return
+	}
+
+	// set up a named pipe for logs also
+	if _, err = initPipe(path.Join(p.dir, "logs")); err != nil {
+		return
+	}
+
+	return
 }
