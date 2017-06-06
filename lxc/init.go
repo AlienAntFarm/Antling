4ad9a4dd40bfab32aa0500c4d0f3f194a9fa541b
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-06 19:43:06 +0200

    Pass options correctly from API to container

--- lxc/init.go
+++ lxc/init.go
@@ -2,21 +2,11 @@ package lxc
 
 import (
 	"archive/tar"
-	"encoding/json"
-	"github.com/alienantfarm/antling/utils"
 	"github.com/golang/glog"
 	"io"
 	"os"
 	"path"
-	"runtime"
 )
-
-type Config struct {
-	Hostname string
-	Env      []string
-	Arch     string
-	RootFS   string
-}
 
 func DeflateLXC(reader io.ReadCloser) error {
 	defer reader.Close()
@@ -33,12 +23,7 @@ func DeflateLXC(reader io.ReadCloser) error {
 		if info.Name() == ".dockerenv" { // ignore .dockerenv file
 			continue
 		}
-		if info.Name() == "manifest.json" { // generate the config
-			if err := deflateConfig(tarReader); err != nil {
-				return err
-			}
-			continue
-		}
+
 		if glog.V(5) { // only for debug, output too much informations
 			glog.Infof("Uncompressing %s, with metadata %q", header.Name, info)
 		}
@@ -67,27 +52,4 @@ func DeflateLXC(reader io.ReadCloser) error {
 
 	}
 	return nil
-}
-
-func deflateConfig(reader io.Reader) error {
-	// build config object
-	decoder := json.NewDecoder(reader)
-	pwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	config := &Config{Arch: runtime.GOARCH, RootFS: pwd}
-
-	if err := decoder.Decode(config); err != nil {
-		return err
-	}
-	glog.Infof(utils.MarshalJSON(config))
-
-	// create file and write it through a template
-	file, err := os.Create(path.Join("..", "config"))
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	return utils.Config.Templates.ConfLXC.Execute(file, config)
 }
--- lxc/runtime.go
+++ lxc/runtime.go
@@ -5,7 +5,9 @@ import (
 	"github.com/alienantfarm/antling/utils"
 	"github.com/golang/glog"
 	"gopkg.in/lxc/go-lxc.v2"
+	"os"
 	"path"
+	"runtime"
 )
 
 var LOG_LEVELS = [...]lxc.LogLevel{
@@ -34,16 +36,16 @@ func Start(job *structs.Job) error {
 	c.SetLogLevel(LOG_LEVELS[utils.LogLevel()]) // pass log level to container
 	c.SetLogFile(path.Join(p.dir, "logs"))
 
-	options := lxc.DefaultAttachOptions
-	options.StdinFd = p.stdin.Fd()
-	options.StdoutFd = p.stdout.Fd()
-	options.StderrFd = p.stderr.Fd()
+	options, err := setupOptions(job, p)
+	if err != nil {
+		return err
+	}
 	if err := c.Start(); err != nil {
 		return err
 	}
 
 	glog.Infof("RunCommand")
-	if _, err := c.RunCommand([]string{"ls"}, options); err != nil {
+	if _, err := c.RunCommand(job.SanitizeCmd(), *options); err != nil {
 		return err
 	}
 
@@ -52,4 +54,46 @@ func Start(job *structs.Job) error {
 		return err
 	}
 	return nil
+}
+
+type Config struct {
+	Hostname string
+	Env      []string
+	RootFS   string
+	Arch     string
+}
+
+func setupOptions(job *structs.Job, pipes *pipes) (*lxc.AttachOptions, error) {
+
+	config := &Config{
+		Env:      job.SanitizeEnv(),
+		Hostname: job.Image.Hostname,
+		RootFS:   path.Join(utils.Config.Paths.LXC, job.Image.Archive),
+		Arch:     runtime.GOARCH,
+	}
+	glog.Infof(utils.MarshalJSON(config))
+
+	// create file and write it through a template
+	if file, err := os.Create(path.Join(config.RootFS, "config")); err != nil {
+		return nil, err
+	} else if err := utils.Config.Templates.ConfLXC.Execute(file, config); err != nil {
+		return nil, err
+	} else {
+		file.Close()
+	}
+
+	glog.Infof(job.SanitizeCwd())
+	return &lxc.AttachOptions{
+		Namespaces: -1,
+		Arch:       -1,
+		Cwd:        job.SanitizeCwd(),
+		UID:        -1,
+		GID:        -1,
+		ClearEnv:   false,
+		Env:        nil,
+		EnvToKeep:  nil,
+		StdinFd:    pipes.stdin.Fd(),
+		StdoutFd:   pipes.stdout.Fd(),
+		StderrFd:   pipes.stderr.Fd(),
+	}, nil
 }
