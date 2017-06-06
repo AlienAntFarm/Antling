f7c6ae7cbf24f50a9a0ece88094e96438881abf1
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-06 18:58:09 +0200

    Parse logs from LXC to prettify for antling

--- lxc/utils.go
+++ lxc/utils.go
@@ -2,6 +2,8 @@ package lxc
 
 import (
 	"bufio"
+	"bytes"
+	"github.com/alienantfarm/antling/utils"
 	"github.com/golang/glog"
 	"io/ioutil"
 	"os"
@@ -14,13 +16,46 @@ type pipes struct {
 	dir                   string
 }
 
-func pipeReading(path string) {
+func extractLog(data []byte) []byte {
+	splitted := bytes.Split(data, []byte{' ', ' ', ' ', ' '})
+	data = (splitted[len(splitted)-1])
+	data = bytes.TrimPrefix(data, []byte{' '})
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
+}
+
+// reuse https://golang.org/src/bufio/scan.go?s=11488:11566#L329
+func ScanLXCLogs(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, extractLog(data[0:i]), nil
+	}
+
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), extractLog(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func pipeReading(path string, splitFunc bufio.SplitFunc) {
 	glog.Infof("opening %s for reading", path)
 	if pipe, err := os.OpenFile(path, os.O_RDONLY, 0600); err != nil {
 		glog.Errorf("opening %s failed, %s", path, err)
 	} else {
 		defer pipe.Close()
 		scanner := bufio.NewScanner(pipe)
+		if splitFunc == nil {
+			splitFunc = bufio.ScanLines
+		}
+		scanner.Split(splitFunc)
 		for scanner.Scan() {
 			glog.Infoln(scanner.Text()) // Println will add back the final '\n'
 		}
@@ -28,11 +63,11 @@ func pipeReading(path string) {
 	}
 }
 
-func initPipe(path string) (*os.File, error) {
+func initPipe(path string, splitFunc bufio.SplitFunc) (*os.File, error) {
 	if err := syscall.Mkfifo(path, 0600); err != nil {
 		return nil, err
 	}
-	go pipeReading(path)
+	go pipeReading(path, splitFunc)
 	glog.Infof("opening for %s writing", path)
 	return os.OpenFile(path, os.O_WRONLY, 0600)
 }
@@ -55,15 +90,15 @@ func newPipes(prefix string) (p *pipes, err error) {
 	}
 	defer utils.RemoveOnFail(p.dir, err)
 
-	if p.stderr, err = initPipe(path.Join(p.dir, "stderr")); err != nil {
+	if p.stderr, err = initPipe(path.Join(p.dir, "stderr"), nil); err != nil {
 		return
 	}
-	if p.stdout, err = initPipe(path.Join(p.dir, "stdout")); err != nil {
+	if p.stdout, err = initPipe(path.Join(p.dir, "stdout"), nil); err != nil {
 		return
 	}
 
 	// set up a named pipe for logs also
-	if _, err = initPipe(path.Join(p.dir, "logs")); err != nil {
+	if _, err = initPipe(path.Join(p.dir, "logs"), ScanLXCLogs); err != nil {
 		return
 	}
 
